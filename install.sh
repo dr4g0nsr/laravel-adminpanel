90e442b82f06795d640dab20f67f7c2a97e5c69b
Dragutin Cirkovic <dragonmen@gmail.com>
2019-05-29 20:30:24 +0000

    fixes

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+function generate_app_key {
+    php -r "echo md5(uniqid()).\"\n\";"
+}
+
 osType=$(uname)
+APP_KEY=$(generate_app_key)
 
 if [[ "$osType" == 'Linux' ]]; then
    sudo chmod -R 777 ./
@@ -9,6 +14,7 @@ fi
 echo -e "\033[32mCopy .env file...\033[0m"
 
 php -r "file_exists('.env') || copy('.env.example', '.env');"
+sed -e s/APP_KEY=.*$/APP_KEY=${APP_KEY}/g .env.example > .env
 echo "-----------------------------------------"
 
 echo -e "\033[32mNpm version:\033[0m"
